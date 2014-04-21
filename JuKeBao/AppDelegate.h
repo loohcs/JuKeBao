d7f2a97298b03ca33f023ce67adec43ca382c5d8
loohcs <loohcs@126.com>
2014-04-21 16:20:38 +0800

    Initial Commit

---
+++ JuKeBao/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JuKeBao
+//
+//  Created by loohcs on 14-4-21.
+//  Copyright (c) 2014年 loohcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
